417a1c05dd07d065240f2b6ddd02e7abdffd1163
DanieSimonLowe <dlo54@uclive.ac.nz>
2024-09-05 10:00:11 +1200

    fixed problem where it did not factor in group permissions in if you can change a permison or not.

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
 from django import forms
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
 
 from home.models import User, Assignment, AssignmentResult
 
@@ -17,6 +17,30 @@ def get_permissions(user):
     group_permissions = set(Permission.objects.filter(group__user=user))
     all_permissions = individual_permissions.union(group_permissions)
     return all_permissions
+
+
+class CustomGroupAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if not request.user.is_superuser:
+            # Get the IDs of permissions the user has
+            user_permission_ids = {perm.id for perm in get_permissions(request.user)}
+            # Get the IDs of groups that have permissions that are a subset of the user's permissions
+            allowed_group_ids = [
+                group.id for group in queryset
+                if set(group.permissions.values_list('id', flat=True)).issubset(set(user_permission_ids))
+            ]
+            queryset = queryset.filter(id__in=allowed_group_ids)
+        return queryset
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            # Limit permissions to the user's own permissions
+            user_permissions = {perm.id for perm in get_permissions(request.user)}
+            form.base_fields['permissions'].queryset = Permission.objects.filter(id__in=user_permissions)
+        return form
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -66,3 +90,6 @@ class CustomUserAdmin(UserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+
+admin.site.unregister(Group)
+admin.site.register(Group, CustomGroupAdmin)
